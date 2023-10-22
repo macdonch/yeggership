ba3f5bd25fe803376c1e0e8d50d795bda1c92236
Charles Macdonald <charles.macdonald@telus.com>
2023-10-22 22:42:08 +0000

    feat: display notes

--- src/client/content/resourcesHome.js
+++ src/client/content/resourcesHome.js
@@ -6,7 +6,7 @@ const ResourcesHome = () => {
 
   return (
     <Box>
-      <Grid container rowSpacing={1} alignItems="top">
+      <Grid container rowSpacing={2} alignItems="top">
         <Grid xs={8} md={10} sx={{ display: 'block' }}>
             <Typography  sx={{ display: 'block' }}>
               This area of the site contains resources that we created for our play sessions, such as:
@@ -17,8 +17,38 @@ const ResourcesHome = () => {
                 <li>house rules, including a modified Panic Table</li>
                 <li>desciptions of hyperspace and terraforming</li>
               </ul>
+            <Typography variant='h4'>
+                SPOILER ALERT
+            </Typography>
             <Typography>
                 Some of the module resources may contain spoilers.
+            </Typography>
+        </Grid>
+        <Grid xs={8} md={10} sx={{ display: 'block' }}>
+        <Typography variant='h5'>
+                PDF Viewing and Printing Tips
+            </Typography>
+            <Typography>
+                Many Mothership pdf files are intended to be viewed as a two page spread, and printed as booklets. Most browsers and Acrobat Reader
+                default to a single page view, and Print settings also usually default to a single page.
+            </Typography>
+            <Typography variant='h6'>
+                Acrobat Reader
+            </Typography>
+            <Typography>
+                Blah blah.
+            </Typography>
+            <Typography variant='h6'>
+                Chrome
+            </Typography>
+            <Typography>
+                At the time of writing, Chrome does give you the option to set the two page view with a seperate cover. You can get around this by using "Print as PDF" to create a coverless copy for viewing.
+            </Typography>
+            <Typography variant='h6'>
+                Printing Booklets
+            </Typography>
+            <Typography>
+                Blah blah.
             </Typography>
         </Grid>
       </Grid>
